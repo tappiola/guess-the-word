642899f31e90a3459f040aba1961c75291ffc518
tappiola <tatiana.karamorina@scandiweb.com>
2022-01-26 15:43:31 +0000

    add modal

--- src/App.svelte
+++ src/App.svelte
@@ -7,16 +7,20 @@
 	import KeypressListener from "./KeypressListener.svelte";
 	import {GAME_STATUS} from "./constants";
 	import {gameStatus, isAnimating, targetWord} from "./stores";
+	import Notification from "./Notification.svelte";
+
+	let showWinModal = false;
+	let showLoseModal = false;
 
 	$: if($gameStatus === GAME_STATUS.WON && !$isAnimating){
-		alert('You won');
+		showWinModal = true;
+		setTimeout(() => {showWinModal = false}, 2000);
 	}
 
 	$: if($gameStatus === GAME_STATUS.LOST && !$isAnimating){
-		alert('You lost '+ $targetWord.toUpperCase());
+		showLoseModal = true;
+		setTimeout(() => {showLoseModal = false}, 2000);
 	}
-
-	$: console.log($targetWord);
 </script>
 
 <main>
@@ -28,6 +32,8 @@
 		<ResetButton/>
 	</div>
 	<KeypressListener/>
+	<Notification showModal={showWinModal}>You won!!!</Notification>
+	<Notification showModal={showLoseModal}>{'You lost '+ $targetWord.toUpperCase()}</Notification>
 </main>
 
 <style>
---
+++ src/Notification.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let showModal;
+</script>
+
+{#if (showModal)}
+    <div class="modal">
+        <slot/>
+    </div>
+{/if}
+
+<style>
+    .modal {
+        position: fixed;
+        width: 300px;
+        background-color: #333333;
+        border-radius: 30px;
+        top: 30px;
+        color: #cccccc;
+        padding: 10px 30px;
+        font-size: 24px;
+    }
+</style>
